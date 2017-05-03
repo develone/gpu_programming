a05c0499389032e9815f4b9677d3b775944638bf
Edward Vidal Jr <develone@sbcglobal.net>
2017-05-03 15:58:45 +0000

    First you need to compile the C fft.c routine arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-$ then create the libfft.a arm-none-eabi-ar rcs libfft.a fft.o I created the kernel.bin on a Ubuntu x86_64 running wine. You can run QEMU from the Lazarus with Tool/Run in QEMU or transfer to RPi
    sudo apt-get install qemu-system-arm
    qemu-system-arm -M versatilepb -cpu cortex-a8 -kernel kernel.bin -m 128

---
+++ Pascal_linked/QEMU/fft.c
@@ -0,0 +1,188 @@
+/* Factored discrete Fourier transform, or FFT, and its inverse iFFT */
+/*
+arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c fft.c
+arm-none-eabi-ar rcs libfft.a fft.o
+*/
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define q	3		/* for 2^3 points */
+#define N	(1<<q)		/* N-point FFT, iFFT */
+
+typedef float real;
+typedef struct{real Re; real Im;} complex;
+
+#ifndef PI
+# define PI	3.14159265358979323846264338327950288
+#endif
+
+
+/* Print a vector of complexes as ordered pairs. */
+static void
+print_vector(
+	     const char *title,
+	     complex *x,
+	     int n)
+{
+  int i;
+  printf("%s (dim=%d):", title, n);
+  for(i=0; i<n; i++ ) printf(" %5.2f,%5.2f ", x[i].Re,x[i].Im);
+  printf("\n");
+  //putchar('\n');
+  return;
+}
+
+
+/* 
+   fft(v,N):
+   [0] If N==1 then return.
+   [1] For k = 0 to N/2-1, let ve[k] = v[2*k]
+   [2] Compute fft(ve, N/2);
+   [3] For k = 0 to N/2-1, let vo[k] = v[2*k+1]
+   [4] Compute fft(vo, N/2);
+   [5] For m = 0 to N/2-1, do [6] through [9]
+   [6]   Let w.re = cos(2*PI*m/N)
+   [7]   Let w.im = -sin(2*PI*m/N)
+   [8]   Let v[m] = ve[m] + w*vo[m]
+   [9]   Let v[m+N/2] = ve[m] - w*vo[m]
+ */
+void
+fft( complex *v, int n, complex *tmp )
+{
+  if(n>1) {			/* otherwise, do nothing and return */
+    int k,m;    complex z, w, *vo, *ve;
+    ve = tmp; vo = tmp+n/2;
+    for(k=0; k<n/2; k++) {
+      ve[k] = v[2*k];
+      vo[k] = v[2*k+1];
+    }
+    fft( ve, n/2, v );		/* FFT on even-indexed elements of v[] */
+    fft( vo, n/2, v );		/* FFT on odd-indexed elements of v[] */
+    for(m=0; m<n/2; m++) {
+      w.Re = cos(2*PI*m/(double)n);
+      w.Im = -sin(2*PI*m/(double)n);
+      z.Re = w.Re*vo[m].Re - w.Im*vo[m].Im;	/* Re(w*vo[m]) */
+      z.Im = w.Re*vo[m].Im + w.Im*vo[m].Re;	/* Im(w*vo[m]) */
+      v[  m  ].Re = ve[m].Re + z.Re;
+      v[  m  ].Im = ve[m].Im + z.Im;
+      v[m+n/2].Re = ve[m].Re - z.Re;
+      v[m+n/2].Im = ve[m].Im - z.Im;
+    }
+  }
+  return;
+}
+
+/* 
+   ifft(v,N):
+   [0] If N==1 then return.
+   [1] For k = 0 to N/2-1, let ve[k] = v[2*k]
+   [2] Compute ifft(ve, N/2);
+   [3] For k = 0 to N/2-1, let vo[k] = v[2*k+1]
+   [4] Compute ifft(vo, N/2);
+   [5] For m = 0 to N/2-1, do [6] through [9]
+   [6]   Let w.re = cos(2*PI*m/N)
+   [7]   Let w.im = sin(2*PI*m/N)
+   [8]   Let v[m] = ve[m] + w*vo[m]
+   [9]   Let v[m+N/2] = ve[m] - w*vo[m]
+ */
+void
+ifft( complex *v, int n, complex *tmp )
+{
+  if(n>1) {			/* otherwise, do nothing and return */
+    int k,m;    complex z, w, *vo, *ve;
+    ve = tmp; vo = tmp+n/2;
+    for(k=0; k<n/2; k++) {
+      ve[k] = v[2*k];
+      vo[k] = v[2*k+1];
+    }
+    ifft( ve, n/2, v );		/* FFT on even-indexed elements of v[] */
+    ifft( vo, n/2, v );		/* FFT on odd-indexed elements of v[] */
+    for(m=0; m<n/2; m++) {
+      w.Re = cos(2*2*PI*m/(double)n);
+      w.Im = sin(2*2*PI*m/(double)n);
+      z.Re = w.Re*vo[m].Re - w.Im*vo[m].Im;	/* Re(w*vo[m]) */
+      z.Im = w.Re*vo[m].Im + w.Im*vo[m].Re;	/* Im(w*vo[m]) */
+      v[  m  ].Re = ve[m].Re + z.Re;
+      v[  m  ].Im = ve[m].Im + z.Im;
+      v[m+n/2].Re = ve[m].Re - z.Re;
+      v[m+n/2].Im = ve[m].Im - z.Im;
+    }
+  }
+  return;
+}
+
+int octave_write_32(const char * fn,complex * d_ptr, int sz) {
+	 
+	FILE *subfileptr;
+	int i;
+	float mag[sz],*ptr;
+	ptr=&mag[0];
+	for(i=0;i<sz;i++) {
+		mag[i]= sqrt(d_ptr[i].Re*d_ptr[i].Re+d_ptr[i].Im*d_ptr[i].Im);
+	}
+	subfileptr = fopen(fn,"w");
+	printf("file name %s data ptr 0x%x size %d \n",fn, d_ptr,sz);
+	if (NULL == subfileptr) {
+		/*
+		fprintf(stderr, "Could not open red for writing\n");
+		perror("RED-WR:");
+		exit(EXIT_FAILURE);
+		*/
+		return(0);
+	}
+ 
+	if (sz != (int)fwrite(ptr,  sizeof(float), sz, subfileptr)) {
+		fprintf(stderr, "Write of red failed\n"); perror("RED:");
+		exit(EXIT_FAILURE);
+	}
+	
+ 	
+	fclose(subfileptr);
+	
+	return(1);
+}
+
+void
+fft_pas(void)
+{
+  complex v[N], v1[N], scratch[N];
+  int k;
+  const char *octave_output_file;
+  
+
+  /* Fill v[] with a function of known FFT: */
+  for(k=0; k<N; k++) {
+    v[k].Re = 0.75*cos(80*2*PI*k/(double)N)+0.25*cos(40*2*PI*k/(double)N);
+    v[k].Im = 0.75*sin(80*2*PI*k/(double)N)+0.25*sin(40*2*PI*k/(double)N);
+    //v1[k].Re =  0.3*cos(2*PI*k/(double)N);
+    //v1[k].Im = -0.3*sin(2*PI*k/(double)N);
+  }
+  octave_output_file = "orig-out.32t";
+  //octave_write_32(octave_output_file,v,N);    
+  /* FFT, iFFT of v[]: */
+  print_vector("Orig", v, N);
+  fft( v, N, scratch );
+  print_vector(" FFT", v, N);
+  octave_output_file = "fft-out.32t";
+  //octave_write_32(octave_output_file,v,N);
+  ifft( v, N, scratch );
+  octave_output_file = "ifft-out.32t";
+  //octave_write_32(octave_output_file,v,N);
+  print_vector("iFFT", v, N);
+
+  /* FFT, iFFT of v1[]: 
+  print_vector("Orig", v1, N);
+  fft( v1, N, scratch );
+  print_vector(" FFT", v1, N);
+  octave_output_file = "fft-out.32t";
+  octave_write_32(octave_output_file,v,N);
+  ifft( v1, N, scratch );
+  print_vector("iFFT", v1, N);
+
+  */
+  //exit(EXIT_SUCCESS);
+}
